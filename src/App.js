4fd829adb8b78da91a7a71803ed663d41a9d3545
Bryce Mercines <nplixel@gmail.com>
2019-12-20 03:37:14 +0800

    initial setup of UI, boilerplate create-react-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          SagiPinas | v 1.0
+        </p>
+        <a
+          className="App-link"
+          href="https://sagipinas.now.sh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coming Soon
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
